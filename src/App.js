eef68504816a4d20cb488448c3c5daf4425e07e9
Source-404 <suyashjohri3@gmail.com>
2023-02-04 19:50:15 +0530

    modified login and register pages

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./Component/Navbar";
 import Coding from "./Pages/Coding";
 import Hackathon from "./Pages/Hackathon";
-import Profile from "./Pages/Register";
+import Register from "./Pages/Register";
 import Login from "./Pages/Login";
 
 function App() {
   return (
     <div className="app">
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" exact element={<Coding />} />
         <Route path="/login" element={<Login />} />
         <Route path="/hackathon" element={<Hackathon />} />
-        <Route path="/register" element={<Profile />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
